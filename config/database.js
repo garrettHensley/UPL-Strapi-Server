59171d6b1bb73a62f07cbab26ff3b434e5f0d97e
Garrett Michael Hensley <garesnap@gmail.com>
2020-07-20 14:42:26 -0400

    init commit

---
+++ config/database.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'dbname'),
+        username: env('DATABASE_USERNAME', 'db_user'),
+        password: env('DATABASE_PASSWORD', 'tonycrub1'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+      options: {}
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3d106e64-dddc-412c-9eab-4611d91457fd'
+};
